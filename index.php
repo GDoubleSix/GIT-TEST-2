5c6ee0dc587dc963153165f664ba961962b5475f
GDoubleSix <muqigewa84@gmail.com>
2024-01-29 23:14:39 +0700

    menambahkan ul li

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia reiciendis nesciunt quaerat! Qui hic similique quia. Quos hic iste praesentium dolor dolore repudiandae, quidem dolores voluptas at repellendus et obcaecati!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
