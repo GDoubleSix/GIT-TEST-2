1f6841eb660334f2aa78c9e71b1e2724fde287fa
GDoubleSix <muqigewa84@gmail.com>
2024-01-29 23:04:15 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia reiciendis nesciunt quaerat! Qui hic similique quia. Quos hic iste praesentium dolor dolore repudiandae, quidem dolores voluptas at repellendus et obcaecati!</p>
 </body>
 </html>
